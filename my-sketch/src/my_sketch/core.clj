a8b3edaa60068d3922823db4f477b755dfb3a435
Kevin Sun <kevsun1@gmail.com>
2021-10-14 16:52:47 -0700

    quil test

--- my-sketch/src/my_sketch/core.clj
+++ my-sketch/src/my_sketch/core.clj
@@ -15,7 +15,7 @@
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
   {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+   :angle (+ (:angle state) 0.3)})
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
@@ -25,7 +25,7 @@
   ; Calculate x and y coordinates of the circle.
   (let [angle (:angle state)
         x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
+        y (* 100 (q/sin angle))]
     ; Move origin point to the center of the sketch.
     (q/with-translation [(/ (q/width) 2)
                          (/ (q/height) 2)]
