6f24c154bfc43988d9238ad727d5d13a3ec63f09
Kevin Sun <kevsun1@gmail.com>
2021-10-29 16:39:42 -0700

    random curves

--- my-sketch/src/my_sketch/test.clj
+++ my-sketch/src/my_sketch/test.clj
@@ -13,14 +13,19 @@
     {:t 0})
   
   (defn update-state [state]
-    ; Update sketch state by changing circle color and position.
     {:t (mod (+ (:t state) (q/random 5 20)) (q/height))})
   
   (defn draw-state [state]
     ; Calculate x and y coordinates of the circle.
     (q/stroke (q/random 255) 255 255)
-    (let [t (:t state)]
-        (q/line [0 t] [t (q/width)])))
+    (q/no-fill)
+    (let [t (:t state) 
+      m (/ (q/width) 2)
+      s (/ m 2)]
+        (q/bezier 0 t 
+          (+ m (* s (q/random-gaussian))) (+ m (* s (q/random-gaussian)))  
+          (+ m (* s (q/random-gaussian))) (+ m (* s (q/random-gaussian)))  
+          t (q/width))))
   
   
   (q/defsketch my-sketch
