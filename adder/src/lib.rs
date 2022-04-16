731abe1da1380234c14fad9e7ee6705423d321f5
Kevin Sun <kevsun1@gmail.com>
2022-04-16 12:53:13 -0700

    integration test examples (book 11.3)

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,13 +1,9 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
 }
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -15,40 +11,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger));
-    }
-
-    #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
-    }
-
 }
