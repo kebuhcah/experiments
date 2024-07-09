44ccbc22b99174f2c2fc7a8be4c8e8713f60af38
Kevin Sun <kevsun1@gmail.com>
2024-07-09 12:30:11 -0700

    Use new high-res images

--- random-ghalib/olive-tree-extension/background.js
+++ random-ghalib/olive-tree-extension/background.js
@@ -1,5 +1,5 @@
 function getRandomUrl() {
-    return `file:///Users/sunkev/Downloads/olive_tree_pages/page-${(16 + Math.floor(Math.random() * 640)).toString().padStart(3, "0")}.jpg`;
+    return `file:///Users/sunkev/Downloads/olive_tree_large/page-${1+(Math.floor(Math.random() * 640))}.jpg`;
 }
 
 chrome.action.onClicked.addListener((tab) => {
