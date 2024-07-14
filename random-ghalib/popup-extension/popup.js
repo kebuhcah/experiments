1adb39108d64ced4abc11d9336c97e48a3e989d5
Kevin Sun <kevsun1@gmail.com>
2024-07-14 11:34:55 -0700

    Implement random Russian page

--- random-ghalib/popup-extension/popup.js
+++ random-ghalib/popup-extension/popup.js
@@ -1,15 +1,15 @@
 'use strict';
 
-function setAlarm(event) {
-  const minutes = parseFloat(event.target.value);
-  chrome.action.setBadgeText({ text: 'ON' });
-  chrome.alarms.create({ delayInMinutes: minutes });
-  chrome.storage.sync.set({ minutes: minutes });
-  window.close();
+function gotoRandomRussianUrl() {
+    const url =  `file:///Users/sunkev/Downloads/russian_synonym_pages/page-${(25+(Math.floor(Math.random() * 542))).toString().padStart(3, "0")}.jpg`;
+    console.log('HEY');
+    chrome.tabs.update({ url });
 }
+
 // An Alarm delay of less than the minimum 1 minute will fire
 // in approximately 1 minute increments if released
-document.getElementById('es').addEventListener('click', setAlarm);
-document.getElementById('fr').addEventListener('click', setAlarm);
-document.getElementById('de').addEventListener('click', setAlarm);
-document.getElementById('ru').addEventListener('click', setAlarm);
+//document.getElementById('de').addEventListener('click', setAlarm);
+//document.getElementById('es').addEventListener('click', setAlarm);
+//document.getElementById('fr').addEventListener('click', setAlarm);
+//document.getElementById('jp').addEventListener('click', setAlarm);
+document.getElementById('ru').addEventListener('click', gotoRandomRussianUrl);
