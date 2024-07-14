971897287cccd8f61f10531e1eb2f8ae3e7d5ea8
Kevin Sun <kevsun1@gmail.com>
2024-07-14 12:04:00 -0700

    Implement all other languages

--- random-ghalib/popup-extension/popup.js
+++ random-ghalib/popup-extension/popup.js
@@ -1,15 +1,34 @@
 'use strict';
 
+function gotoRandomFrenchUrl() {
+    const url =  `file:///Users/sunkev/Downloads/french_synonym_pages/page-${(18+(Math.floor(Math.random() * 465))).toString().padStart(3, "0")}.jpg`;
+    chrome.tabs.update({ url });
+}
+
+function gotoRandomGermanUrl() {
+    const url =  `file:///Users/sunkev/Downloads/german_synonym_pages/page-${(29+(Math.floor(Math.random() * 253))).toString().padStart(3, "0")}.jpg`;
+    chrome.tabs.update({ url });
+}
+
+function gotoRandomJapaneseUrl() {
+    const url =  `file:///Users/sunkev/Downloads/japanese_synonym_pages/page-${(18+(Math.floor(Math.random() * 390))).toString().padStart(3, "0")}.jpg`;
+    chrome.tabs.update({ url });
+}
+
 function gotoRandomRussianUrl() {
     const url =  `file:///Users/sunkev/Downloads/russian_synonym_pages/page-${(25+(Math.floor(Math.random() * 542))).toString().padStart(3, "0")}.jpg`;
-    console.log('HEY');
+    chrome.tabs.update({ url });
+}
+
+function gotoRandomSpanishUrl() {
+    const url =  `file:///Users/sunkev/Downloads/spanish_synonym_pages/page-${(19+(Math.floor(Math.random() * 582))).toString().padStart(3, "0")}.jpg`;
     chrome.tabs.update({ url });
 }
 
 // An Alarm delay of less than the minimum 1 minute will fire
 // in approximately 1 minute increments if released
-//document.getElementById('de').addEventListener('click', setAlarm);
-//document.getElementById('es').addEventListener('click', setAlarm);
-//document.getElementById('fr').addEventListener('click', setAlarm);
-//document.getElementById('jp').addEventListener('click', setAlarm);
+document.getElementById('fr').addEventListener('click', gotoRandomFrenchUrl);
+document.getElementById('de').addEventListener('click', gotoRandomGermanUrl);
+document.getElementById('jp').addEventListener('click', gotoRandomJapaneseUrl);
 document.getElementById('ru').addEventListener('click', gotoRandomRussianUrl);
+document.getElementById('es').addEventListener('click', gotoRandomSpanishUrl);
